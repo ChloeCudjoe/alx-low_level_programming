b0ae46a998440488de2538e5c7185091725828c9
ChloeCudjoe <huldahakwetey@gmail.com>
2022-10-13 16:47:40 -0700

    writing our first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,26 +1,12 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - prints "Programming is like building a multilingual puzzle
- *
- *   * @void: Function argument
- *
- *    * Return: 0 if the code runs successfuly. 1 otherwise
- *
- *     */
-
-
-
+ * main - prints Programming is like building a multilingual puzzle
+ * @void: Function argument
+ * Return: 0 if the code runs succesfully, otherwise 1
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
-
-}			
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
