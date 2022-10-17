1c50e02c77dda49fdd63a7ce29a43958421b7628
ChloeCudjoe <huldahakwetey@gmail.com>
2022-10-17 13:03:33 -0700

    checking betty style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers go there */
+/**
+ 
+ * main function to geneate a random number
+ *
+ * Return: Always zero when success
+ *
+ */
 
-/* betty style doc for function main goes there */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes here */
+	n = rand() - RAND_MAX / 2
 
 	if (n > 0)
 	{
